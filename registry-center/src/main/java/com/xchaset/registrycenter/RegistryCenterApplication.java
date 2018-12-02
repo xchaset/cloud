c45092c12204c0e923eeb6a1a424abd127e57aac
xchaset <943314903@qq.com>
2018-12-02 13:59:49 +0800

    first commit  add eureka and zuul api-gateway

---
+++ registry-center/src/main/java/com/xchaset/registrycenter/RegistryCenterApplication.java
@@ -0,0 +1,12 @@
+package com.xchaset.registrycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryCenterApplication.class, args);
+	}
+}
